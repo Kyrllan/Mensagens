1af6602bd304588f8ca68c4867fe2cc0b61a2e12
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-10-13 17:05:06 -0300

    upload project

---
+++ Mensagens/BaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Kyrllan Nogueira on 08/05/19.
+//  Copyright © 2019 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    @IBOutlet weak var lbMEssage: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeColor(_ sender: UIButton) {
+        
+        
+    }
+
+}
